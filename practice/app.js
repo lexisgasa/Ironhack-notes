7d05e8844e2d02e06dedc8cc024e5b5c0ffebe2f
Alexis <lexisgasa3007@gmail.com>
2023-01-18 16:17:31 +0100

    Organization

--- practice/app.js
+++
@@ -1,7 +0,0 @@
-const bioCheck = (firstName, age, livesIn) => {
-    return `${firstName} tiene ${age} años y vive en ${
-      livesIn.livesInCity ? "la ciudad" : "el pueblo"
-    }`;
-  };
- 
-  console.log(bioCheck("Martina", 42, { livesInCity: false, livesInTown: true }));
